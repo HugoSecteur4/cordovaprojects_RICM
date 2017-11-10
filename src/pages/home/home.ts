20d0d6c750a4720642d9a20745037c55cd6abb3d
HugoGDA <gdahugo@gmail.com>
2017-11-10 17:11:03 +0100

    question 10

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface IResultRecherche {
+  title: string;
+  author: string;
+  date: string;
+  image: string;
+}
+
+const liste: IResultRecherche[] = [{ title: "denis", author: "denis l'auteur", date: "29 février 2018", image:"http://lorempixel.com/400/200/"},
+                                       { title: "damien", author: "damien l'auteur", date: "31 octobre 1944", image: "http://lorempixel.com/400/200/" },
+                                       { title: "dimitri", author: "dimitri l'auteur", date: "27 septembre 2012", image: "http://lorempixel.com/400/200/" }];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  liste: IResultRecherche[] = liste;
+  query: string = "";
 
-  constructor(public navCtrl: NavController) {
-
+  getItems(ev: any) {
+    console.log(this.query);
   }
+
 
 }
